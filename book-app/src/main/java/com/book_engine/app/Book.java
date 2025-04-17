5c53efe7a5277171abf2150c58c867f952e1c7a7
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-04-17 13:40:41 +0200

    feat: add combined string method to book-app/src/main/java/com/book_engine/app/Book.java

--- book-app/src/main/java/com/book_engine/app/Book.java
+++ book-app/src/main/java/com/book_engine/app/Book.java
@@ -9,16 +9,19 @@ public class Book {
     public String average_rating;
     public ArrayList<String> genres;
     public Float rating = null;
+    public String combinedString;
+    public double score = 0;
 
     @Override
     public String toString() {
-        return "Book{title='" + this.title + "', genres=" + this.genres + "rating= " + this.rating + "}";
+        // return "Book{title='" + this.title + "', genres=" + this.genres + "rating= " + this.rating + "}";
+        return this.combinedString;
     }
 
     /**
      * Function to handle null ratings 
      */
-    public void setRating(){
+    public void setRating() {
         try {
             this.rating = Float.parseFloat(this.average_rating);
         } catch (Exception e){
@@ -26,6 +29,19 @@ public class Book {
             System.out.println("Was unable to find rating for book: " + this.title);
         }
        
+    }
+
+    /**
+     * Function to combine description, genre and title into one big string (a document)
+     */
+    public void setCombinedString()  {
+        try {
+            this.combinedString = this.description + this.title + String.join(" ", this.genres);
+        } catch (Exception e){
+            this.combinedString = null;
+        }
+
+
     }
 
    
