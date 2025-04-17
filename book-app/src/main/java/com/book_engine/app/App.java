91bca3164d1d4191d53ed12cad4423efb4a9fe1d
Elin Fransholm <157385515+efransholm@users.noreply.github.com>
2025-04-17 13:10:59 +0200

    feat: Add user class (#15)
    User class stores read books and ratings. Refectored some code in Database and App to be more concise. Added two tests for adding books to user.
    
    closes #14
    
    Co-authored-by: Alexander Runebou <Firverior02@users.noreply.github.com>

--- book-app/src/main/java/com/book_engine/app/App.java
+++ book-app/src/main/java/com/book_engine/app/App.java
@@ -1,13 +1,16 @@
 package com.book_engine.app;
+import com.book_engine.app.User;
 
 /**
- * Hello world!
+ * Search engine
  */
 public class App {
     public static void main(String[] args) {
-        // System.out.println("Hello World!");
-        // Database.readData();
-        System.out.println(Tokenizer.tokenize("It is the middle of the summer 11 (Rowling, 2007), but there is an unseasonal mist pressing against the windowpanes"));
+        System.out.println("Search engine is starting indexing...");
+        Database database = new Database();
+        database.readData();
 
+        // TEST tokenization
+        // System.out.println(Tokenizer.tokenize("It is the middle of the summer 11 (Rowling, 2007), but there is an unseasonal mist pressing against the windowpanes"));
     }
 }
