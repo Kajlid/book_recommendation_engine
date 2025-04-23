c32b4b03cdbb51ec957098aa3736d80c3fe6ade1
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-04-23 10:13:49 +0200

    style: remove commented out code

--- book-app/src/main/java/com/book_engine/app/App.java
+++ book-app/src/main/java/com/book_engine/app/App.java
@@ -10,8 +10,5 @@ public class App {
         Database database = new Database();
         database.readData();
 
-        // TEST tokenization
-        // System.out.println(Tokenizer.tokenize("It is the middle of the summer 11 (Rowling, 2007), but there is an unseasonal mist pressing against the windowpanes"));
-
     }
 }
