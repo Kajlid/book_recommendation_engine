74d703f2cb6c76ef58403e1d90acd32ae63c9f8e
efransholm <elin.fransholm@gmail.com>
2025-04-23 14:05:36 +0200

    feat: update visuals in GUI
    Retrieves book content when selecting a book
    
    Co-authored-by: Alexander Runebou <Firverior02@users.noreply.github.com>

--- book-app/src/main/java/com/book_engine/app/GUI.java
+++ book-app/src/main/java/com/book_engine/app/GUI.java
@@ -20,45 +20,48 @@ public class GUI extends JFrame {
     JMenu userMenu = new JMenu("User");
     JMenuItem quitItem = new JMenuItem("Quit");
 
-    public JPanel resultWindow = new JPanel();
-    public JTextArea docTextView = new JTextArea(); //The content of a specified book
-    private JScrollPane resultPane = new JScrollPane(resultWindow); // Contains the search results 
-    private JScrollPane docViewPane = new JScrollPane(docTextView);
-    Database database = null;
+    // Main panel that contains all subpanels
+    //JPanel mainPanel = new JPanel(new BorderLayout(20,30));  
+    JPanel mainPanel = new JPanel(new BorderLayout(10,10)); 
+    //Panel for search results
+    JPanel searchResultPanel = new JPanel(); 
+    // Pane with search results and scroll bar inside searchResultPanel
+    JScrollPane searchResultPane = new JScrollPane(searchResultPanel); 
+
+    // TextArea for book content and description after selected
+    JTextArea bookContentTextArea = new JTextArea(); 
+    // Pane inside bookContextArea to be able to scroll through description of a booj
+    JScrollPane bookContentPane = new JScrollPane(bookContentTextArea);
+    
+    
+    Database database;
 
     public GUI(Database database) {
         this.database = database;
+
+        // Set up the window attributes
         setTitle("Book recommendation system");
-        setSize(600, 650);
+        setSize(800, 800);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocationRelativeTo(null); // Center the window
+        setLocationRelativeTo(null); 
         
         
-        JPanel mainPanel = new JPanel(new BorderLayout());
+        // Initialize main panel attributes and add to window
         mainPanel.setBorder(new EmptyBorder(20, 20, 20, 20));
         add(mainPanel);
 
-        // Search: input field and search button
-        JTextField searchInput = new JTextField(16);
-        Action search = new AbstractAction() {
-            public void actionPerformed( ActionEvent e ) {
-                // Empty the results window
-                resultWindow.removeAll();
-                JLabel label = new JLabel( "info" );
-                resultWindow.add( label );
-                revalidate();
-                repaint();
-            }
-        };
-        searchInput.registerKeyboardAction( search,
-                            "",
-                            KeyStroke.getKeyStroke( "ENTER" ),
-                            JComponent.WHEN_FOCUSED );
+        
 
+        // Search: input field and search button
+        JTextField searchInput = new JTextField(40);
+        Action searchAction = getSearchAction();
+
+        searchInput.registerKeyboardAction( searchAction, 
+                            "", 
+                            KeyStroke.getKeyStroke( "ENTER" ), 
+                            JComponent.WHEN_FOCUSED );
                             
         JButton searchButton = new JButton("search");
-        mainPanel.add(searchInput, BorderLayout.CENTER);
-        mainPanel.add(searchButton, BorderLayout.LINE_END);
 
         // Set up menu
         menuBar.add(searchMenu);
@@ -66,56 +69,71 @@ public class GUI extends JFrame {
         menuBar.add(quitItem);
         setJMenuBar(menuBar);
 
-        resultWindow.setLayout(new BoxLayout(resultWindow, BoxLayout.Y_AXIS));
-        resultPane.setLayout(new ScrollPaneLayout());
-        resultPane.setBorder( new EmptyBorder(10,10,10,0) );
-        resultPane.setPreferredSize( new Dimension(400, 450));
-        docViewPane.setPreferredSize( new Dimension(400, 100));
 
+        searchResultPanel.setLayout(new BoxLayout(searchResultPanel, BoxLayout.Y_AXIS));
+        searchResultPane.setLayout(new ScrollPaneLayout());
+        searchResultPanel.setBorder( new EmptyBorder(10,10,10,10) );
+        searchResultPane.setPreferredSize( new Dimension(400, 450));
+        bookContentPane.setPreferredSize( new Dimension(400, 200));
 
-        docTextView.setText("\n  The contents of the document will appear here.");
-        docTextView.setLineWrap(true);
-        docTextView.setWrapStyleWord(true);
-        docTextView.setEditable(false); // non-editable content of book 
+        bookContentTextArea.setText("\n  The contents of the document will appear here.");
+        bookContentTextArea.setFont(new Font("Times New Roman",Font.BOLD, 14));
+        bookContentTextArea.setLineWrap(true);
+        bookContentTextArea.setWrapStyleWord(true);
+        bookContentTextArea.setEditable(false); // non-editable content of book 
+        bookContentTextArea.setBorder( new EmptyBorder(10,10,10,10) );
 
         // Action for quitting
         quitItem.addActionListener(e -> System.exit(0));
 
+        // Page title
         JLabel welcomeLabel = new JLabel("Welcome to the book recommendation engine");
+        welcomeLabel.setFont(new Font("Times New Roman", Font.BOLD, 16));
         
         // Logo
-        JPanel p1 = new JPanel(); // First JPanel 
-        p1.setPreferredSize(new Dimension(120, 50));
+        JPanel p1 = new JPanel(new BorderLayout(120, 50)); // First JPanel 
         p1.add(welcomeLabel, BorderLayout.PAGE_START);
         p1.add(searchInput, BorderLayout.LINE_START);
-        p1.add(searchButton, BorderLayout.LINE_END);
+        p1.add(searchButton, BorderLayout.CENTER);
         mainPanel.add(p1, BorderLayout.PAGE_START);
 
         ArrayList<Book> books = new ArrayList<Book>();
+        // TODO: get books from database
         books.add(database.getBookByID(0));
         books.add(database.getBookByID(1));
         books.add(database.getBookByID(2));
 
 
-        JScrollPane resultPane = displayBookResults(books); // get search result Pane
-        resultWindow.add(resultPane);
+        JScrollPane searchResultPane = displayBookResults(books); // get search result Pane
+        searchResultPanel.add(searchResultPane);
 
-        
-        resultWindow.setOpaque(true);
-        resultWindow.setBackground(Color.WHITE);
-        resultPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        searchResultPanel.setOpaque(true);
+        searchResultPanel.setBackground(Color.WHITE);
+        searchResultPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-
-        //p2.add(docViewPane, BorderLayout.CENTER);
-        mainPanel.add(resultPane, BorderLayout.CENTER);
-
-        JPanel p3 = new JPanel();
-        
-        p3.add(docViewPane);
-        mainPanel.add(p3, BorderLayout.PAGE_END);
+        mainPanel.add(searchResultPane, BorderLayout.CENTER);
+        mainPanel.add(bookContentPane, BorderLayout.PAGE_END);
        
         setVisible(true); // Make it visible
     }
+
+    /**
+     * Search action
+     */
+    private Action getSearchAction() {
+        return new AbstractAction() {
+            public void actionPerformed( ActionEvent e ) {
+                // Empty the results window
+                searchResultPanel.removeAll();
+                
+                // Create a new label and display it
+                JLabel label = new JLabel("info");
+                searchResultPanel.add(label);
+                revalidate();
+                repaint();
+            }
+        };
+    } 
 
 
     /**
@@ -124,33 +142,29 @@ public class GUI extends JFrame {
     public JScrollPane displayBookResults(ArrayList<Book> books){
         DefaultListModel<String> listModel = new DefaultListModel<>();
         for (int i = 0; i < books.size(); i++) {
-            listModel.addElement(i + " " + books.get(i).title);
+            listModel.addElement(i + " " + books.get(i).title + " - " + books.get(i).author);
         }
 
         JList<String> resultList = new JList<>(listModel);
+
         resultList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         resultList.setLayoutOrientation(JList.VERTICAL);
         resultList.setVisibleRowCount(-1); 
         resultList.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-        resultList.setFont(new Font("SansSerif", Font.PLAIN, 14));
+        resultList.setFont(new Font("Times New Roman", Font.PLAIN, 16));
         resultList.addListSelectionListener(e -> {
             if (!e.getValueIsAdjusting()) {
                 String selected = resultList.getSelectedValue();
                 Integer booknr = Integer.parseInt(selected.split(" ")[0]);
                 displayBookContent(books.get(booknr));
-                //System.out.println("User selected: " + selected);
             }
         });
-        JScrollPane newResultPane = new JScrollPane(resultList);
-        newResultPane.setPreferredSize(new Dimension(400, 450));
-        return newResultPane;
+
+        JScrollPane newsearchResultPane = new JScrollPane(resultList);
+        return newsearchResultPane;
     }
 
     public void displayBookContent(Book book){
-        docTextView.setText(book.displayContents());
-        repaint();
-        revalidate();
+        bookContentTextArea.setText(book.displayContents());
     }
-
-    
 }
