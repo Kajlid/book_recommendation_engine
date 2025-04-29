d02ee0475f56aea51a74b6bc912a599b79e12598
efransholm <elin.fransholm@gmail.com>
2025-04-29 15:36:20 +0200

    feat: Added user data
    -updated gitignore to include user data files
    -changed title2id to only be used to check for existing books
    - updated more concise function in User class
    - added function that reads all user data
    
    closes #26

--- book-app/src/main/java/com/book_engine/app/User.java
+++ book-app/src/main/java/com/book_engine/app/User.java
@@ -15,21 +15,6 @@ public class User{
      * @param db Database object to get the book id
      * @param rating User rating of the book
      */
-    public void addBook(Book book, Database db, Float rating){ //TODO: might be able to delete this function and only use addBookById
-        // get book object id
-        String stringid = db.generateId(book);
-
-        // Handle invalid id
-        if (stringid == null){
-            return;
-        }
-        Book bookdb = db.getBookByID(stringid); // get same book object as in index
-        if (bookdb == null){
-            return;
-        }
-        books.put(stringid, rating);
-    }
-
     public void addBookById(String bookid, Database db, Float rating){
         Book bookdb = db.getBookByID(bookid); // get same book object as in index
         if (bookdb == null){
